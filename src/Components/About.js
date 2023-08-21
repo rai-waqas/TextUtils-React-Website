e0df88e0174ddfa154db60368ed79a41a4703835
Rai <raiwaqas515@gmail.com>
2023-08-21 22:32:22 +0500

    Setup_Routes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function About() {
+export default function About(props) {
   return (
     <>
-        <div className='container my-3'>
-            <h1 className='container my-2'>About Us</h1>
+        <div className='container my-3' style={{color: props.mode==='dark'?'white':'black' }}>
+            <h1 className='container my-2' >About Us</h1>
                 <p className='container my-2'>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
                     Itaque tempora suscipit esse iure, dicta repudiandae dolorum inventore magni. 
